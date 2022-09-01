fccc56f3187db9b21ea9a980895a30415384a8a8
Kosmonaut 3173 <re.huerta3173@gmail.com>
2022-08-31 22:06:22 -0500

    modified js code to get color coding to work

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
 }
+setInterval(displayTime, 1000);
 
 $(document).ready(function() {
+
     //save button click listener for user input. want time and text
     $(".saveBtn").on("click", function() {
         //get values
@@ -23,30 +25,29 @@ $(document).ready(function() {
     $("#hr10 .description").val(localStorage.getItem("hr10"));
     $("#hr11 .description").val(localStorage.getItem("hr11"));
     $("#hr12 .description").val(localStorage.getItem("hr12"));
-    $("#hr1 .description").val(localStorage.getItem("hr1"));
-    $("#hr2 .description").val(localStorage.getItem("hr2"));
-    $("#hr3 .description").val(localStorage.getItem("hr3"));
-    $("#hr4 .description").val(localStorage.getItem("hr4"));
-    $("#hr5 .description").val(localStorage.getItem("hr5"));
-
+    $("#hr13 .description").val(localStorage.getItem("hr13"));
+    $("#hr14 .description").val(localStorage.getItem("hr14"));
+    $("#hr15 .description").val(localStorage.getItem("hr15"));
+   
     //color coding hrs
     function hourTracker() {
-        //get current time
+        //get current time in 24hr format
         var currentHour = moment().hour();
-
+        
+        //to compare to current time, get block by id name then remove hr leaving just hour
         $(".time-block").each(function() {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            var blockHour = parseInt($(this).attr("id").split("hr")[1]);
             console.log(blockHour, currentHour)
+            
 
             //link to css past, pressent, future classes
             if (blockHour < currentHour) {
                 $(this).addClass("past");
-                $(this).removeClass("present");
-                $(this).removeClass("future");
-            } else if (blockHour === currentHour) {
+                
+            } else if (blockHour == currentHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
-                $(this).removeClass("future");
+                
             } else {
                 $(this).removeClass("past");
                 $(this).removeClass("present");
@@ -56,6 +57,3 @@ $(document).ready(function() {
     }
     hourTracker();
 })
-
-
-setInterval(displayTime, 1000);
